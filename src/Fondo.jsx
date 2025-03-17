6a288493d5a6ddc48f24fb2070eea63dd4034ec4
josanva777 <johansanchezvalencia@gmail.com>
2025-03-17 12:57:10 -0500

    lista la tarea de posiciones de componentes

--- src/Fondo.jsx
+++ src/Fondo.jsx
@@ -1,15 +1,45 @@
+import React from "react";
 import CuadroRosa from "./CuadroRosa";
 import CuadroNaranja from "./CuadroNaranja";
 
 export function Fondo() {
   return (
-    <div className="fondo">
-      <CuadroRosa />  
-      <CuadroNaranja />
-      <CuadroNaranja />
-      <CuadroRosa /> 
-      <CuadroRosa />   
-      <CuadroNaranja /> 
+    <div className="contenedor-principal">
+                                                    {/* contenedor izquierdo */}
+      <div className="fondo fondo-izquierdo">
+        <div className="fila fila1">
+          <CuadroRosa />
+          <CuadroNaranja />
+        </div>
+
+        <div className="fila fila2">
+          <CuadroNaranja />
+          <CuadroRosa />
+        </div>
+
+        <div className="fila fila3">
+          <CuadroRosa />
+          <CuadroNaranja />
+        </div>
+      </div>
+
+                                                  {/* Contenedor Derecho*/}
+      <div className="fondo fondo-derecho">
+        <div className="fila fila1-espejo">
+          <CuadroNaranja />
+          <CuadroRosa />
+        </div>
+
+        <div className="fila fila2-espejo">
+          <CuadroRosa />
+          <CuadroNaranja />
+        </div>
+
+        <div className="fila fila3-espejo">
+          <CuadroNaranja />
+          <CuadroRosa />
+        </div>
+      </div>
     </div>
   );
 }
